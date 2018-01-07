5b5d50fcc0e523e69e7180128e631a0069b15281
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-01-08 01:57:26 +0200

    Added ErrorMessage

--- EsportTournaments.Core/WebConstants.cs
+++ EsportTournaments.Core/WebConstants.cs
@@ -7,6 +7,7 @@
         public const string TournamentModeratorRole = "Tournament Moderator";
 
         public const string TempDataSuccessMessageKey = "SuccessMessage";
+        public const string TempDataErrorMessageKey = "ErrorMessage";
 
         public const string AdminArea = "Admin";
         public const string ModeratorArea = "Moderator";
